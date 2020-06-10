33b1622cb944799a1b9f1f55498055853f4eb60d
Ashish Kujur <ashish.xenon@gmail.com>
2020-06-11 00:06:21 +0530

    Implimented "page-mapping" to avoid loading of same page.
    Page-mapping, basically checks if the section you are looking for exists in the same page. If it does, avoid redirection and simply display the section, if it doesn't , then redirect to that specific page and that specific section.

--- js/script.js
+++ js/script.js
@@ -16,14 +16,41 @@ $(document).ready(function(){
   });
 });
 
-// NAVIGATING TO DIFFERENT SECTIONS OF THE PAGE (CAROUSEL)
+// Function to tab switch among different sections of different pages
 $(document).ready(function(){
   let searchParams = new URLSearchParams(window.location.search);
   searchParams.has('tab');
   let section = searchParams.get('tab');
-  $('#'+section).toggleClass('active');
+  $('.carousel-grid > div').removeClass('active');
+  $('#'+section).addClass('active');
 });
 
+// Function to tab switch among same page sections
+function switchTab(tab_id)
+{
+  $('.carousel-grid > div').removeClass('active');
+  $('#'+tab_id).addClass('active');
+}
+
+// Function to check if the tab you want to access is in the same page or different page
+$(document).ready(function(){
+  $('a').on("click",function(e){
+    let cur_url = window.location.href; // current url
+    let cur_link = cur_url.split('?')[0].split('/')[4]; // current html file
+    console.log(cur_link);
+    let url = $(this).attr('href'); // url to redirect
+    let red_link = url.split('?')[0]; // html file to load.
+    let red_tab = url.split('?')[1].split('=')[1]; // section to load.
+    if(cur_link == red_link) // if both the html pages match that means the section to load is in the same page.
+    {
+      e.preventDefault(); // prevent redirection
+      switchTab(red_tab); // load section
+      const state = { 'page_id': 1, 'user_id': 5 };  // push it in the history and update the url
+      const title = 'Student Welfare Office, MNNIT';
+      history.pushState(state, title, url);
+    }
+  });
+});
 // POPUP INTERACTIONS
 function popup(i)
 {
